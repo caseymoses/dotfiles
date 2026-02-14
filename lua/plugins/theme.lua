db6e762db50ac55cbceb90733e719cced385d027
Casey Moses <caseymoses@Caseys-Mac-mini.local>
2026-02-14 17:24:54 -0600

    Switch theme from Nordic to Catppuccin Mocha

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,29 +1,23 @@
--- return { -- You can easily change to a different colorscheme.
--- 	-- Change the name of the colorscheme plugin below, and then
--- 	-- change the command in the config to whatever the name of that colorscheme is.
--- 	--
--- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
--- 	"folke/tokyonight.nvim",
--- 	priority = 1000, -- Make sure to load this before all the other start plugins.
--- 	init = function()
--- 		-- Load the colorscheme here.
--- 		-- Like many other themes, this one has different styles, and you could load
--- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
--- 		vim.cmd.colorscheme("tokyonight-storm")
---
--- 		-- You can configure highlights by doing something like:
--- 		vim.cmd.hi("Comment gui=none")
--- 	end,
--- }
-
 return {
-	"AlexvZyl/nordic.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("nordic").load()
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "white" })
-		-- vim.api.nvim_set_v(0, "NormalFloat", { bg = "white" })
-		vim.cmd("hi Visual  guifg=#000000 guibg=#88D0C0 gui=none")
+	opts = {
+		flavour = "mocha",
+		transparent_background = false,
+		integrations = {
+			gitsigns = true,
+			telescope = true,
+			treesitter = true,
+			which_key = true,
+			mini = true,
+			neo_tree = true,
+			indent_blankline = { enabled = true },
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
